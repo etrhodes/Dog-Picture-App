f9c5431e0fa07276d531875eb9f9674f0773f4c6
Eileen Rhodes <etrhodes619@gmail.com>
2020-10-20 18:06:55 -0600

    initial upload

---
+++ app.js
@@ -0,0 +1,33 @@
+function getDogImage () {
+    let numberOfImages= $('input[type="number"]').val();
+    fetch('https://dog.ceo/api/breeds/image/random/')
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+        .catch(error => alert ('Something went wrong. Try again later'));
+}
+
+
+
+function onSubmit () {
+    $('form').on('submit', event => {
+        event.preventDefault();
+        let numberOfImages = $('input[type="number"]').val();       
+        getDogImage(numberOfImages);
+    })
+}
+
+function displayImages(responseJson) {
+    $('.img-result').replaceWith(   
+    '<img src="${responseJson.message}" class="img-result">'
+    )
+    $('.results').removeClass('hidden');
+
+}
+
+function handler() {
+    onSubmit();
+}
+
+handler();
+
+
